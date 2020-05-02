1ee772dc114493b5dffdcfa00afd9a58c978be02
johan guxman <johan.dev19@gmail.com>
2020-05-02 09:55:59 -0400

    add useEffect to categories list

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -5,18 +5,38 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(( ) => {
     fetch('https://petgram-server-mikpc3u6g.now.sh/categories')
       .then(res => res.json())
       .then(response => setCategories(response))
   }, [])
+  
+  useEffect(() => {
+    console.log(window.screenY )
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
 
-  return (
-    <List>
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+ 
+  const renderList = (fixed) => {
+    return (<List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
-    </List>
+    </List>)
+  }
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
