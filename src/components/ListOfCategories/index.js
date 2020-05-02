bbcd71d274b03cdf7a257a8bb714cabb7700c87d
johan guxman <johan.dev19@gmail.com>
2020-05-02 17:31:41 -0400

    adding loading fetching data

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,42 +1,63 @@
-import React, {useEffect, useState} from 'react'
-import { Category } from './../Category'
+import React, { useEffect, useState } from "react";
+import { Category } from "./../Category";
 
-import { List, Item } from './styles'
+import { List, Item } from "./styles";
+
+function useCategoriesData() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://petgram-server-mikpc3u6g.now.sh/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        setLoading(false);
+        setCategories(response);
+      });
+  }, []);
+
+  return { categories, loading };
+}
 
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState([]);
+  const { categories, loading } = useCategoriesData();
   const [showFixed, setShowFixed] = useState(false);
 
-  useEffect(( ) => {
-    fetch('https://petgram-server-mikpc3u6g.now.sh/categories')
-      .then(res => res.json())
-      .then(response => setCategories(response))
-  }, [])
-  
   useEffect(() => {
-    console.log(window.screenY )
-    const onScroll = e => {
-      const newShowFixed = window.scrollY > 200
-      showFixed !== newShowFixed && setShowFixed(newShowFixed)
-    }
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
 
-    document.addEventListener('scroll', onScroll)
+    document.addEventListener("scroll", onScroll);
 
-    return () => document.removeEventListener('scroll', onScroll)
-  }, [showFixed])
- 
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
   const renderList = (fixed) => {
-    return (<List className={fixed ? 'fixed' : ''}>
-      {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-      }
-    </List>)
-  }
+    return (
+      <List fixed={fixed}>
+        {loading ? (
+          <Item key="loading">
+            <Category/>
+          </Item>
+        ) : (
+          categories.map((category) => (
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          ))
+        )}
+      </List>
+    );
+  };
 
   return (
     <>
       {renderList()}
       {showFixed && renderList(true)}
     </>
-  )
-}
+  );
+};
