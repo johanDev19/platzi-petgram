62fc2a5a500af45b86080148f3d97c245b5f6e90
johan guxman <johan.dev19@gmail.com>
2020-05-02 08:27:47 -0400

    add photoCard component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import { ListOfCategories } from './components/ListOfCategories'
-import { PhotoCard } from './components/PhotoCard'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyle />
     <ListOfCategories />
-    <PhotoCard />
+    <ListOfPhotoCards />
   </>
 )
