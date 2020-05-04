091476b0490287b2e1ce67cf0fbfd22f8a3d48a5
johan guxman <johan.dev19@gmail.com>
2020-05-04 19:55:32 -0400

    add render props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
-import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
-import { Logo } from './components/Logo'
-import { GlobalStyle } from './styles/GlobalStyles'
+import { ListOfCategories } from "./components/ListOfCategories";
+import { ListOfPhotoCards } from "./container/ListOfPhotoCards";
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
+import { Logo } from "./components/Logo";
+import { GlobalStyle } from "./styles/GlobalStyles";
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get("detail");
+
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <Fragment>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={1} />
+        </Fragment>
+      )}
+    </>
+  );
+};
