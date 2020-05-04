2bd31bd7054e32cc3eb40a4b0f50318170a9e372
johan guxman <johan.dev19@gmail.com>
2020-05-04 19:12:02 -0400

    add graphsql to list photos card component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: "https://petgram-server-mikpc3u6g.now.sh/graphql",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
