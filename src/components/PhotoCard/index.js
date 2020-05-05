049e3cd1fac338ccaeac7706ef1007fa19174271
johan guxman <johan.dev19@gmail.com>
2020-05-04 23:07:51 -0400

    use react apollo mutation to increment the like numbers

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,21 @@
-import React, { useRef, useEffect, useState, Fragment } from "react";
-import { ImgWrapper, Img, Button, Article } from "./styles";
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import React, { Fragment } from "react";
+import { ImgWrapper, Img, Article } from "./styles";
 
-import { useLocalStorage } from './../../hooks/useLocalStorage';
-import { useNearScreen } from './../../hooks/useNearScreen';
+import { useLocalStorage } from "./../../hooks/useLocalStorage";
+import { useNearScreen } from "./../../hooks/useNearScreen";
+import { ToggleLikeMutation } from "./../../container/ToggleLikeMutation";
+
+import { FavButton } from "./../FavButton";
 
 const DEFAULT_IMAGE =
   "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
+  const key = `like-${id}`;
 
   const [liked, setLiked] = useLocalStorage(key, false);
   const [show, element] = useNearScreen();
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
-  
   return (
     <Article ref={element}>
       {show && (
@@ -25,10 +25,29 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
+          <ToggleLikeMutation>
+            {(toggleLike) => {
+              const handleFavButton = () => {
+                !liked &&
+                  toggleLike({
+                    variables: {
+                      input: {
+                        id,
+                      },
+                    },
+                  });
+                setLiked(!liked);
+              };
 
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size="32px" /> {likes} likes!
-          </Button>
+              return (
+                <FavButton
+                  liked={liked}
+                  likes={likes}
+                  onClick={handleFavButton}
+                />
+              );
+            }}
+          </ToggleLikeMutation>
         </Fragment>
       )}
     </Article>
